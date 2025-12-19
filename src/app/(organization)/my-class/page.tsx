365d77eb0ba194966c361e79049365eac09ca35b
duyzuy <vutruongduy2109@gmail.com>
2025-12-19 15:53:49 +0700

    Fix learner screen

--- src/app/(organization)/my-class/page.tsx
+++ src/app/(organization)/my-class/page.tsx
@@ -3,22 +3,22 @@ import PageContainer from "@/shared/ui/PageContainer";
 import MyClassSection from "./_components";
 
 const MyClassPage = () => {
-    return (
-        <PageContainer
-            title="Lớp học của tôi"
-            breadcrumbs={[
-                {
-                    title: "LMS",
-                    path: "/dashboard",
-                },
-                {
-                    title: "Lớp học của tôi",
-                },
-            ]}
-        >
-            <MyClassSection />
-        </PageContainer>
-    );
-}
+  return (
+    <PageContainer
+      title="Lớp học của tôi"
+      breadcrumbs={[
+        {
+          title: "LMS",
+          path: "/dashboard",
+        },
+        {
+          title: "Lớp học của tôi",
+        },
+      ]}
+    >
+      <MyClassSection />
+    </PageContainer>
+  );
+};
 
 export default MyClassPage;
